64e84c1aafb48aab629372d82215384d432e7065
princeavo <avohouprince@gmail.com>
2024-12-07 19:56:54 +0100

    rspec tests added

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,26 @@
 class Task < ApplicationRecord
     validates :title, presence: true
     validates :content, presence: true
+
+    validates :deadline_on, presence: true
+    validates :priority, presence: true
+    validates :status, presence: true
+
+
+    enum priority: {
+        Faible: 0,
+        Moyenne: 1,
+        Elevée: 2 
+    }
+    enum status:{
+        "Non démarré": 0,
+        "Démarré": 1,
+        "Terminé":2
+    }
+
+    scope :search_title, -> title {where("title LIKE ?", "%#{title}%")}
+    scope :search_status, -> status {where(status: status)}
+    scope :order_by_created_at, -> {order(created_at: :desc,id: :desc)}
+    scope :order_by_deadline, -> {order(deadline_on: :asc)}
+    scope :order_by_priority_desc, -> {order(priority: :desc)}
 end
---
+++ db/migrate/20241201184744_add_deadline_to_tasks.rb
@@ -0,0 +1,5 @@
+class AddDeadlineToTasks < ActiveRecord::Migration[6.1]
+  def change
+    add_column :tasks, :deadline_on, :date, null: false
+  end
+end
